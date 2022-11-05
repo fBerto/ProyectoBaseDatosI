161e9ac4eb4d2482dc5d00aaa7a6c6a5b1d6c788
Franco Bertoni <franco.bertoni@alumnos.ucse.edu.ar>
2022-11-05 12:30:22 -0300

    .

--- WindowsFormsBaseDatosI/WindowsFormsBaseDatosI/Form1.Designer.cs
+++ WindowsFormsBaseDatosI/WindowsFormsBaseDatosI/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,7 +71,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(161, 24);
             this.button3.TabIndex = 3;
-            this.button3.Text = "Procedimiento Almacenado";
+            this.button3.Text = "Resumen Proveedores";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.ProcedimientoAlmacenado_Click);
             // 
@@ -83,11 +84,21 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Tabla de los Obreros";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Agregar ";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(558, 318);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -109,6 +120,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
